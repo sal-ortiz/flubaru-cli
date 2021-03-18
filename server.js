f3561b5cbe2eb118cc7b3df249d219549de6d26f
pilot <this.is.salvador.ortiz@gmail.com>
2021-03-18 11:51:45 -0700

    addressed a few typos in server script

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 
 const Args = require('command-line-args');
+const PortAudio = require('naudiodon');
 const TCP = require('tcp-audio-stream').Server;
+
 
 const defaultAddr = '0.0.0.0';
 const defaultPort = '3000';
@@ -13,12 +15,13 @@ const exitCodeFailure = 255;
 const argsCfg = [
   { name: 'address',  alias: 'b', type: String, defaultOption: defaultAddr },
   { name: 'port',     alias: 'p', type: String, defaultOption: defaultPort },
+  { name: 'help',     alias: 'h', type: String, defaultOption: defaultPort },
 ]
 
 const args = Args(argsCfg);
 
 
-if (!(args.bind) || !(args.dest)) {
+if (args.help) {
   let str = [
     'usage: ./server.js [BIND_ADDRESS] [BIND_PORT]',
     '   [BIND_ADDRSS] defaults to address ' + defaultAddr,
@@ -29,7 +32,6 @@ if (!(args.bind) || !(args.dest)) {
   process.stdout.write(str);
   process.exit(exitCodeFailure);
 }
-
 
 
 const outpEngine = new PortAudio.AudioIO({
@@ -47,15 +49,18 @@ const outpEngine = new PortAudio.AudioIO({
 });
 
 
-const client = new Client(inpEngine);
+const server = new TCP(outpEngine);
 
-process.on('exit', client.disconnect.bind(client));       // cleanup
+process.on('exit', server.stop.bind(server));       // cleanup
 
 process.on('SIGINT', process.exit.bind(process));   // exit
 process.on('SIGUSR1', process.exit.bind(process));  // exit
 process.on('SIGUSR2', process.exit.bind(process));  // exit
 
 
-server.start(args.address, args.port);
+let tcpAddress = args.address || defaultAddr;
+let tcpPort = args.port || defaultPort;
 
-process.exit(exitcodeSuccess);
+server.start(tcpAddress, tcpPort);
+
+process.exit(exitCodeSuccess);
