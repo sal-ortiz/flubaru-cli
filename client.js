e030b9f5fca06243479c84523c2142f95c94dfd1
sal ortiz <this.is.salvador.ortiz@gmail.com>
2021-06-14 12:06:58 -0500

    removed args parsing in server/client

--- client.js
+++ client.js
@@ -4,34 +4,12 @@ const Args = require('command-line-args');
 const PortAudio = require('naudiodon');
 const UDP = require('udp-audio-stream');
 
-//const defaultAddr = '0.0.0.0';
-const defaultAddr = '224.0.0.120';
+const defaultAddr = '0.0.0.0';
+//const defaultAddr = '224.0.0.120';
 const defaultPort = 3000;
 
 const exitCodeSuccess = 0;
 const exitCodeFailure = 255;
-
-
-const argsCfg = [
-  { name: 'port',     alias: 'p', type: Number, defaultOption: defaultPort },
-  { name: 'address',  alias: 'b', type: String, defaultOption: defaultAddr },
-  { name: 'help',     alias: 'h', type: String },
-]
-
-const args = Args(argsCfg);
-
-
-if (args.help) {
-  let str = [
-    'usage: ./client.js [TARGET_PORT] [TARGET_ADDRESS]',
-    '   [TARGET_PORT defaults to port ' + defaultPort,
-    '   [TARGET_ADDRSS] defaults to address ' + defaultAddr,
-    '',
-  ].join('\n');
-
-  process.stdout.write(str);
-  process.exit(exitCodeFailure);
-}
 
 
 const inpEngine = new PortAudio.AudioIO({
@@ -57,11 +35,8 @@ process.on('SIGUSR1', process.exit.bind(process));    // exit
 process.on('SIGUSR2', process.exit.bind(process));    // exit
 
 
-let udpAddress = args.address;
-let udpPort = args.port;
+let udpAddress = process.argv[2] || defaultAddr;
+let udpPort = (process.argv[1] && parseInt(process.argv[1])) || defaultPort;
 
-console.log(args)
-//console.log(udpAddress, udpAddress.constructor)
-//console.log(udpPort, udpPort.constructor)
 
-client.connect(args.address, args.port);
+client.connect(udpAddress, udpPort);
--- server.js
+++ server.js
@@ -4,35 +4,12 @@ const Args = require('command-line-args');
 const PortAudio = require('naudiodon');
 const UDP = require('udp-audio-stream');
 
-
-//const defaultAddr = '0.0.0.0';
-const defaultAddr = '224.0.0.120';
+const defaultAddr = '0.0.0.0';
+//const defaultAddr = '224.0.0.120';
 const defaultPort = 3000;
 
 const exitCodeSuccess = 0;
 const exitCodeFailure = 255;
-
-
-const argsCfg = [
-  { name: 'port',     alias: 'p', type: Number, defaultOption: defaultPort },
-  { name: 'address',  alias: 'b', type: String, defaultOption: defaultAddr },
-  { name: 'help',     alias: 'h', type: String },
-]
-
-const args = Args(argsCfg);
-
-
-if (args.help) {
-  let str = [
-    'usage: ./server.js [BIND_PORT] [BIND_ADDRESS]',
-    '   [BIND_PORT defaults to port ' + defaultPort,
-    '   [BIND_ADDRSS] defaults to address ' + defaultAddr,
-    '',
-  ].join('\n');
-
-  process.stdout.write(str);
-  process.exit(exitCodeFailure);
-}
 
 
 const outpEngine = new PortAudio.AudioIO({
@@ -59,7 +36,8 @@ process.on('SIGUSR1', process.exit.bind(process));  // exit
 process.on('SIGUSR2', process.exit.bind(process));  // exit
 
 
-let udpAddress = args.address;
-let udpPort = args.port;
+let udpAddress = process.argv[2] || defaultAddr;
+let udpPort = (process.argv[1] && parseInt(process.argv[1])) || defaultPort;
 
-server.start(args.address, args.port);
+
+server.start(udpAddress, udpPort);
